7af8eb5d1a111c8835c07f9539132da0b75d1d1b
fahterodin <fahterodin@protonmail.com>
2022-03-09 15:19:05 +0100

    Fixed delete method and added find method

--- lib/node.rb
+++ lib/node.rb
@@ -3,12 +3,19 @@ require_relative 'comparable'
 class Node
   include Comparable
 
-  attr_reader :data
-  attr_accessor :right, :left
+  attr_accessor :right, :left, :data
 
   def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
+  end
+
+  def min_value
+    if @left.nil?
+      @data
+    else
+      @left.min_value
+    end
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -40,42 +40,49 @@ class Tree
     end
   end
 
-  def delete(value, node = @root, parent = nil, direction = '')
+  def delete(value, node = @root, parent = nil)
+    # three cases
+    # if the node is a leaf, just remove its link from the parent
+    # if the node has one child, just let its parent link to its child
+    # if the node has two children, look at its right subtree, find the lowest value there(= node with no left child) remove that node (using case 2) and use its value in the current node
     return if node.nil?
 
-    if value == @root.data
-      return @root = Node.new(node.right.data, node.left, node.right.right)
-    end
-
     if value < node.data
-      delete(value, node.left, node, 'left')
+      delete(value, node.left, node)
     elsif value > node.data
-      delete(value, node.right, node, 'right')
-    elsif node.right.nil? && node.left.nil?
-      if direction == 'left'
-        parent.left = nil
-      else
-        parent.right = nil
-      end
-    elsif node.left.nil?
-      if direction == 'left'
-        parent.left = node.right
-      else
-        parent.right = node.right
-      end
+      delete(value, node.right, node)
+    # we have found the node with the value
+    elsif node.left.nil? && node.right.nil?
+      parent.left = nil if parent.left == node
+      parent.right = nil if parent.right == node
     elsif node.right.nil?
-      if direction == 'left'
-        parent.left = node.left
-      else
-        parent.right = node.left
-      end
+      parent.right = node.left if parent.right == node
+      parent.left = node.left if parent.left == node
+    elsif node.left.nil?
+      parent.right = node.right if parent.right == node
+      parent.left = node.right if parent.left == node
     else
-      if direction == 'left'
-        parent.left = Node.new(node.right.data, node.left)
-      else
-        parent.right = Node.new(node.right.data, node.left)
-      end
+      min = node.right.min_value
+      delete(min)
+      node.data = min
     end
+  end
+
+  def find(value, node = @root)
+    if value < node.data
+      find(value, node.left)
+    elsif value > node.data
+      find(value, node.right)
+    else
+      node
+    end
+  end
+
+  def level_order(&block)
+    # traverse the three in breadth-first level order and yeld each node to the block
+    # if no block is given it should return an array of values of all the nodes passed
+    # we also have a queue array
+    # iteration and recursion possible
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -89,3 +96,4 @@ tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
 tree.pretty_print
 tree.delete(4)
 tree.pretty_print
+p tree.find(6)
